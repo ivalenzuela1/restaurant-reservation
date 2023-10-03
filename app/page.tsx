39790bf424138a487c929da777098245d4b0090f
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-02 17:31:35 -0700

    Add cards

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,32 @@ export default function Home() {
             </div>
           </div>
           {/* HEADER */}
+          {/* CARDS */}
+          <div className="py-3 px-36 mt-10 flex flex-wrap">
+            {/* CARD */}
+            <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
+              <img
+                src="https://resizer.otstatic.com/v2/photos/wide-huge/3/48569639.webp"
+                alt=""
+                className="w-full h36"
+              />
+              <div className="p1">
+                <h3 className="font-bold text-2xl mb-2">Milestone Grill</h3>
+                <div className="flex items-start">
+                  <div className="flex mb-2">*****</div>
+                  <p className="ml-2">77 Reviews</p>
+                </div>
+                <div className="flex text-reg font-light capitalize">
+                  <p className="mr-3">Mexican</p>
+                  <p className="mr-3">$$$</p>
+                  <p className="mr-3">Toronto</p>
+                </div>
+                <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
+              </div>
+            </div>
+            {/* CARD */}
+          </div>
+          {/* CARDS */}
         </main>
       </main>
     </main>
