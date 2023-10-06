6ef7f1fe18220bfaf34fa41a431770268f155299
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-06 14:25:48 -0700

    Add query params by using the Link component

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,35 +1,101 @@
+import { Cuisine, Locality, PRICE } from "@prisma/client";
+import Link from "next/link";
 import React from "react";
 
-export default function SearchSideBar() {
+export default async function SearchSideBar({
+  locations,
+  cuisines,
+  searchParams,
+}: {
+  locations: Locality[];
+  cuisines: Cuisine[];
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const prices = [
+    {
+      price: PRICE.CHEAP,
+      label: "$",
+      className: "border w-full text-reg text-center font-light rounded-l p-2",
+    },
+    {
+      price: PRICE.REGULAR,
+      label: "$$",
+      className: "border w-full text-reg text-center font-light p-2",
+    },
+    {
+      price: PRICE.EXPENSIVE,
+      label: "$$$",
+      className: "border w-full text-reg text-center font-light rounded-r p-2",
+    },
+  ];
+
   return (
-    <div className="w-1/5">
-      <div className="border-b pb-4">
-        <h1 className="mb-2">Region</h1>
-        <p className="font-light text-reg">Toronto</p>
-        <p className="font-light text-reg">Ottawa</p>
-        <p className="font-light text-reg">Montreal</p>
-        <p className="font-light text-reg">Hamilton</p>
-        <p className="font-light text-reg">Kingston</p>
-        <p className="font-light text-reg">Niagra</p>
-      </div>
-      <div className="border-b pb-4 mt-3">
-        <h1 className="mb-2">Cuisine</h1>
-        <p className="font-light text-reg">Mexican</p>
-        <p className="font-light text-reg">Italian</p>
-        <p className="font-light text-reg">Chinese</p>
-      </div>
-      <div className="mt-3 pb-4">
-        <h1 className="mb-2">Price</h1>
-        <div className="flex">
-          <button className="border w-full text-reg font-light rounded-l p-2">
-            $
-          </button>
-          <button className="border w-full text-reg font-light p-2">$$</button>
-          <button className="border-r border-t border-b w-full text-reg font-light rounded-r p-2">
-            $$$
-          </button>
+    <>
+      <div className="w-1/5">
+        <div className="border-b pb-4 flex flex-col">
+          <h1 className="mb-2">Region</h1>
+          {locations.map((location) => {
+            return (
+              <Link
+                href={{
+                  pathname: "/search",
+                  query: {
+                    ...searchParams,
+                    city: location.name,
+                  },
+                }}
+                className="font-light text-reg capitalize"
+                key={location.id}
+              >
+                {location.name}
+              </Link>
+            );
+          })}
+        </div>
+        <div className="border-b pb-4 mt-3 flex flex-col">
+          <h1 className="mb-2">Cuisine</h1>
+          {cuisines.map((cuisine) => {
+            return (
+              <Link
+                href={{
+                  pathname: "/search",
+                  query: {
+                    ...searchParams,
+                    cuisine: cuisine.name,
+                  },
+                }}
+                className="font-light text-reg capitalize"
+                key={cuisine.id}
+              >
+                {cuisine.name}
+              </Link>
+            );
+          })}
+        </div>
+        <div className="mt-3 pb-4">
+          <h1 className="mb-2">Price</h1>
+          <div className="flex">
+            {prices.map(({ price, label, className }) => {
+              return (
+                <>
+                  <Link
+                    href={{
+                      pathname: "/search",
+                      query: {
+                        ...searchParams,
+                        price,
+                      },
+                    }}
+                    className={className}
+                  >
+                    {label}
+                  </Link>
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -40,20 +40,36 @@ const fetchRestaurantsByLocation = (
   });
 };
 
+const fetchLocations = () => {
+  return prisma.locality.findMany();
+};
+
+const fetchCuisines = () => {
+  return prisma.cuisine.findMany();
+};
+
 export default async function Search({ searchParams }: Props) {
   const city = searchParams.city as string;
   const restaurants = await fetchRestaurantsByLocation(city);
+  const locations = await fetchLocations();
+  const cuisines = await fetchCuisines();
 
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSideBar />
+        <SearchSideBar
+          locations={locations}
+          cuisines={cuisines}
+          searchParams={searchParams}
+        />
         <div className="w-5/6">
           {restaurants.length ? (
             <>
               {restaurants.map((restaurant) => {
-                return <RestaurantCard restaurant={restaurant} />;
+                return (
+                  <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+                );
               })}
             </>
           ) : (
