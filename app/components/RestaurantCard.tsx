3fd8f8477cdb97a3f7e194a49a683dedc641b735
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-05 19:36:11 -0700

    fetch restaurant data from db and pass as props

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,23 +1,23 @@
 import Link from "next/link";
 import React from "react";
 
-export default function RestaurantCard() {
+interface Props {
+  restaurant: RestaurantCardType;
+}
+
+export default function RestaurantCard({ restaurant }: Props) {
   return (
     <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
       <Link href="/restaurant/milestone-grill">
-        <img
-          src="https://resizer.otstatic.com/v2/photos/wide-huge/3/48569639.webp"
-          alt=""
-          className="w-full h36"
-        />
+        <img src={restaurant.main_image} alt="" className="w-full h36" />
         <div className="p1">
-          <h3 className="font-bold text-2xl mb-2">Milestone Grill</h3>
+          <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
-            <div className="flex mb-2">*****</div>
+            <div className="flex mb-2">{restaurant.price}</div>
             <p className="ml-2">77 Reviews</p>
           </div>
           <div className="flex text-reg font-light capitalize">
-            <p className="mr-3">Mexican</p>
+            <p className="mr-3">{restaurant.cuisine.name}</p>
             <p className="mr-3">$$$</p>
             <p className="mr-3">Toronto</p>
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
+import { PrismaClient } from "@prisma/client";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      //  location: true,
+      price: true,
+    },
+  });
+
+  return restaurants;
+};
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants();
+
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap">
-        <RestaurantCard />
+        {restaurants.map((restaurant: RestaurantCardType) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
     </main>
   );
