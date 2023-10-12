4be66c87c6fef3ce61aedb86d98387f6ca131cf0
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-12 14:59:00 -0700

    Implement Sign out functionality

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,17 +1,42 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
+import useAuth from "../../hooks/useAuth";
+import { AuthenticationContext } from "../context/AuthContext";
 import AuthModal from "./AuthModal";
 
 export default function NavBar() {
+  const { loading, data } = useContext(AuthenticationContext);
+  const { signout } = useAuth();
+
   return (
     <nav className="bg-white p-2 flex justify-between">
       <Link href="/" className="font-bold text-gray-700 text-2xl">
         Open Table
       </Link>
-      <div className="flex">
-        <AuthModal isSignIn={true} />
-        <AuthModal isSignIn={false} />
-      </div>
+      {loading ? null : (
+        <div className="flex">
+          {data ? (
+            <>
+              <p className="mr-4 flex items-center font-bold">
+                Hi, {data?.firstName}
+              </p>
+              <button
+                className="bg-blue-400 text-white border p-1 px-4 rounded mr-3"
+                onClick={signout}
+              >
+                Sign out
+              </button>
+            </>
+          ) : (
+            <>
+              <AuthModal isSignIn={true} />
+              <AuthModal isSignIn={false} />
+            </>
+          )}
+        </div>
+      )}
     </nav>
   );
 }
