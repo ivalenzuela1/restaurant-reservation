23d2afb60bf9b8f4bf7222f004b81a1ceaf3ebcc
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-17 14:46:11 -0700

    Add logic to determine which tables to book

--- app/api/restaurant/[slug]/reserve/route.ts
+++ app/api/restaurant/[slug]/reserve/route.ts
@@ -71,7 +71,50 @@ export async function GET(
     });
   }
 
-  return NextResponse.json({ searchTimeWithTables });
+  const tablesCount: {
+    2: number[];
+    4: number[];
+  } = {
+    2: [],
+    4: [],
+  };
+
+  searchTimeWithTables.tables.forEach((table) => {
+    if (table.seats === 2) {
+      tablesCount[2].push(table.id);
+    } else {
+      tablesCount[4].push(table.id);
+    }
+  });
+
+  const tablesToBook: number[] = [];
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      } else {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      } else {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      }
+    }
+  }
+
+  return NextResponse.json({ tablesToBook, tablesCount });
 }
 
 // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2023-10-16&time=15:00:00.000Z&partySize=4
