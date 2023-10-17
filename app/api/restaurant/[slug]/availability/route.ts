6db9a019db6422ac48cd011f607157c6eb23bed9
Ignacio Valenzuela <ignaciovalen1@gmail.com>
2023-10-17 09:02:51 -0700

    Filter available times by open/close time of the restaurant

--- app/api/restaurant/[slug]/availability/route.ts
+++ app/api/restaurant/[slug]/availability/route.ts
@@ -61,6 +61,8 @@ export async function GET(
     },
     select: {
       tables: true,
+      open_time: true,
+      close_time: true,
     },
   });
 
@@ -91,22 +93,29 @@ export async function GET(
     });
   });
 
-  const availabilities = searchTimesWithTables.map((t) => {
-    const sumSeats = t.tables.reduce((sum, table) => {
-      return sum + table.seats;
-    }, 0);
+  const availabilities = searchTimesWithTables
+    .map((t) => {
+      const sumSeats = t.tables.reduce((sum, table) => {
+        return sum + table.seats;
+      }, 0);
 
-    return {
-      time: t.time,
-      available: sumSeats >= parseInt(partySize),
-    };
-  });
+      return {
+        time: t.time,
+        available: sumSeats >= parseInt(partySize),
+      };
+    })
+    .filter((availability) => {
+      const timeIsAfterOpeningHour =
+        new Date(`${day}T${availability.time}`) >=
+        new Date(`${day}T${restaurant.open_time}`);
+      const timeIsBeforeClosingHour =
+        new Date(`${day}T${availability.time}`) <=
+        new Date(`${day}T${restaurant.close_time}`);
+      return timeIsAfterOpeningHour && timeIsBeforeClosingHour;
+    });
 
   return NextResponse.json({
     availabilities,
-    bookingTablesObj,
-    tables,
-    searchTimesWithTables,
   });
 }
 
